464e1d9679cd52ef05323ea81fa4d830e6f6f2bb
AlexGNovo <alex_guno0110@hotmail.com>
2022-10-17 20:03:14 -0500

    Services  Component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -4,7 +4,7 @@ import {BiCheck} from 'react-icons/bi'
 
 const Services = () => {
   return (
-    <section id="services">
+    <section id='services'>
       <h5> What I offer </h5>
       <h2> Services </h2>
 
@@ -35,11 +35,13 @@ const Services = () => {
               <BiCheck className="service__list-icon" />
               <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
             </li>
-            <ul/>
-          <article/>
+            </ul>
+          </article>
          {/*================= End of UI/UX===============*/}
+
+
          <article className="service">
-         <div className="service head">
+         <div className="service__head">
             <h3>Web Development</h3>
           </div>
 
@@ -64,8 +66,8 @@ const Services = () => {
               <BiCheck className="service__list-icon" />
               <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
             </li>
-            <ul/>
-            <article/>
+            </ul>
+            </article>
          {/*================= Content Creation ===============*/}
           
          <article className="service">
@@ -94,9 +96,12 @@ const Services = () => {
             </li>
           </ul>         
         </article>
-        
-    </section>
-  );
-};
 
-export default Services;
+        {/* End of Content Creation */}
+
+        </div>
+    </section>
+    )
+    }
+
+export default Services
